26df4f6d712dd3f6a4d285876cd14952bb162135
Joel Boersma <joelboersma@gmail.com>
2019-08-01 16:19:12 -0700

    Basic UI layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoteComponent } from './components/note/note.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
