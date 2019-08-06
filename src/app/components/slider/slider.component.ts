d296f2f9d1b885341ab1c4c1292aba7e2e7721cc
Joel Boersma <joelboersma@gmail.com>
2019-08-06 14:49:55 -0700

    Notes play now yay

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Options, LabelType, CustomStepDefinition } from 'ng5-slider';
+import { Options, ChangeContext } from 'ng5-slider';
+import { Synth } from 'tone';
 
 @Component({
   selector: 'app-slider',
@@ -8,6 +9,7 @@ import { Options, LabelType, CustomStepDefinition } from 'ng5-slider';
 })
 export class SliderComponent implements OnInit {
   vals: Array<string> = ["None", "Hold", 'G', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'A', 'B', 'C', 'D', 'E', '?'];
+  synth = new Synth().toMaster();
 
   value: number = 0;
   options: Options = {
@@ -17,8 +19,8 @@ export class SliderComponent implements OnInit {
     vertical: true,
     hideLimitLabels: true,
 
-    translate: (value: number, label: LabelType): string => {
-      return this.numberToNote(value);
+    translate: (value: number): string => {
+      return this.numberToLabel(value);
     }
   };
 
@@ -27,15 +29,47 @@ export class SliderComponent implements OnInit {
   ngOnInit() {
   }
 
-  numberToNote(num: number): string {
+  numberToLabel(num: number): string {
     return this.vals[num];
   }
 
-  onUserChangeStart() {
-    
+  numberToNote(num: number): string {
+    switch(num) {
+      case 0:  return 'None';
+      case 1:  return 'Hold';
+      case 2:  return 'G3';
+      case 3:  return 'A3';
+      case 4:  return 'B3';
+      case 5:  return 'C4';
+      case 6:  return 'D4';
+      case 7:  return 'E4';
+      case 8:  return 'F4';
+      case 9:  return 'G4';
+      case 10: return 'A4';
+      case 11: return 'B4';
+      case 12: return 'C5';
+      case 13: return 'D5';
+      case 14: return 'E5';
+      case 15: return 'Random';
+      default: return 'Error';
+    }
   }
 
-  onUserChange() {
+  playNote(note: string): void {
+    if (note.length === 2) {
+      this.synth.triggerAttackRelease(note, '8n');
+    }
+  }
+
+  onUserChangeStart(changeContext: ChangeContext): void {
+    this.playNote(this.numberToNote(changeContext.value))
+  }
+
+  onUserChange(changeContext: ChangeContext): void {
+    this.playNote(this.numberToNote(changeContext.value))
+  }
+
+  onUserChangeEnd(changeContext: ChangeContext): void {
 
   }
 }
