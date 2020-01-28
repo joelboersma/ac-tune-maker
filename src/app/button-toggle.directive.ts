b278753bc67efa79f31d4814374a8c0a0d1280fc
Joel Boersma <joelboersma@gmail.com>
2020-01-27 23:32:20 -0800

    button-toggle works

--- src/app/button-toggle.directive.ts
+++ src/app/button-toggle.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input, EventEmitter, HostListener } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appButtonToggle]'
 })
 export class ButtonToggleDirective {
+  @Input('appButtonToggle') reenableButton: EventEmitter<boolean>;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+  
+  ngOnInit() {
+    this.subscription = this.reenableButton.subscribe(value => {
+      if (!value) this.renderer.removeAttribute(this.el.nativeElement, 'disabled');
+    })
+  }
 
+  @HostListener('click')
+  onClick() {
+    this.renderer.setAttribute(this.el.nativeElement, 'disabled', 'true');
+  }
+
+  ngOnDestroy() {
+    this.subscription && this.subscription.unsubscribe();
+  }
 }
